6d972b5271fc95d75d15d891e5788fdc599c769b
parisPP <73128372+parisPP@users.noreply.github.com>
2026-07-26 13:58:57 +0200

    postcards

---
+++ walkingperson-content.js
@@ -0,0 +1,63 @@
+const sharedWalkingPersonHTML = `
+  <div class="popup-content dama-dot-design">
+   <button class="close-popup">✕</button>
+    
+    <div class="project-title">
+      <h2>WALKING PERSON</h2>
+      <p class="project-subtitle">Postcard collection in collaboration with <a href="https://wenttothemoon.com/" target="_blank">Jonas Skovmand</a>, spring 2026.</p>
+    </div>
+
+
+  
+    
+    <div class="portfolio-collage">
+      
+      <div class="collage-item span-2">
+        <img src="img-pc/pc2.jpg" loading="lazy">
+      </div>
+      
+      <div class="collage-item span-3 nudge-down">
+        <img src="img-pc/pc1.jpg" loading="lazy">
+      </div>
+
+          <div class="break"></div>
+      <div class="collage-item spacer"></div>
+      
+      <div class="collage-item span-2 nudge-left">
+        <img src="img-pc/pc3.jpg" loading="lazy">
+      </div>
+
+      <div class="collage-item span-3">
+        <img src="img-pc/pc4.jpg" loading="lazy">
+      </div>
+
+       <div class="collage-item span-4">
+        <img src="img-pc/pc5.jpg" loading="lazy">
+      </div>
+
+      <div class="collage-item span-2 nudge-down">
+        <img src="img-pc/pc6.jpg" loading="lazy">
+      </div>
+
+      <div class="break"></div>
+
+      <div class="collage-item span-3 nudge-right">
+        <img src="img-pc/pc7.jpg" loading="lazy">
+      </div>
+
+       <div class="collage-item span-3 edge-up">
+        <img src="img-pc/pc8.jpg" loading="lazy">
+      </div>
+
+        <div class="break"></div>
+
+        <div class="collage-item span-3 nudge-down">
+        <img src="img-pc/pc9.jpg" loading="lazy">
+      </div>
+
+      
+
+    </div>
+  </div>
+
+`;
